03ed8f538e19f6e1977cbb8abc0a59a0df147db0
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-09 20:39:29 +0900

    feat: BusinessException 생성

--- src/main/java/com/budgetguard/global/error/ErrorCode.java
+++ src/main/java/com/budgetguard/global/error/ErrorCode.java
@@ -5,6 +5,9 @@ import org.springframework.http.HttpStatus;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * 비즈니스 로직 예외 메세지 및 상태 코드
+ */
 @Getter
 @RequiredArgsConstructor
 public enum ErrorCode {
