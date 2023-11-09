c910a3939bf156fd2200815886134f098e398a39
mizuirohoshi7 <mizuirohoshi7@gmail.com>
2023-11-09 19:54:51 +0900

    Initial commit

---
+++ src/main/java/com/budgetguard/BudgetGuardApplication.java
@@ -0,0 +1,13 @@
+package com.budgetguard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetGuardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetGuardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/budgetguard/BudgetGuardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budgetguard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetGuardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
